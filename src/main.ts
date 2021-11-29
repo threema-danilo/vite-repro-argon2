883bff8230e76a7fad670c3105e8464423300ae3
Danilo Bargen <danilo.bargen@threema.ch>
2021-11-29 18:49:01 +0100

    Add argon2

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import App from './App.svelte'
 
+import * as argon2 from 'argon2';
+
 const app = new App({
   target: document.getElementById('app')
 })
+
+argon2.hash('asdf');
 
 export default app
